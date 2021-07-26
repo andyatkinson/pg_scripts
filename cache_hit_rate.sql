305c818437af36b9998ac8ca84f22fc86515d88a
Andy Atkinson <andyatkinson@gmail.com>
2021-07-26 09:28:14 -0500

    Cache hit rate

---
+++ cache_hit_rate.sql
@@ -0,0 +1,7 @@
+-- https://www.craigkerstiens.com/2012/10/01/understanding-postgres-performance/
+SELECT 
+  sum(heap_blks_read) as heap_read,
+  sum(heap_blks_hit)  as heap_hit,
+  sum(heap_blks_hit) / (sum(heap_blks_hit) + sum(heap_blks_read)) as ratio
+FROM 
+  pg_statio_user_tables;
