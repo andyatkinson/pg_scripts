4a88f696f4a03f2be3fd888d0351e2278b057b44
Andy Atkinson <andyatkinson@gmail.com>
2023-01-14 22:21:46 -0600

    List 10 largest tables

---
+++ list_10_largest_tables.sql
@@ -0,0 +1,12 @@
+-- List 10 largest tables
+SELECT
+  schemaname AS table_schema,
+  relname AS table_name,
+  pg_size_pretty(pg_total_relation_size(relid)) AS total_size,
+  pg_size_pretty(pg_relation_size(relid)) AS data_size,
+  pg_size_pretty(pg_total_relation_size(relid) - pg_relation_size(relid)) AS external_size
+FROM pg_catalog.pg_statio_user_tables
+ORDER BY
+  pg_total_relation_size(relid) DESC,
+  pg_relation_size(relid) DESC
+LIMIT 10;
