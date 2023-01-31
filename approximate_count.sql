c877e200821bb1a109eebf57f4fb057437ae7b58
Andrew Atkinson <andyatkinson@gmail.com>
2023-01-31 12:52:08 -0600

    Add more to approximate count

--- approximate_count.sql
+++ approximate_count.sql
@@ -1,2 +1,5 @@
 -- Approximate row counts
 SELECT relname, relpages, reltuples::numeric, relallvisible, relkind, relnatts, relhassubclass, reloptions, pg_table_size(oid) FROM pg_class WHERE relname='table';
+
+-- Simplified
+SELECT reltuples::numeric FROM pg_class WHERE relname='table';
