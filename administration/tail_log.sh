2d96268c0471716a7ac41da85b2b9f3b5dc81023
Andy Atkinson <andyatkinson@gmail.com>
2023-12-13 22:13:50 -0600

    Script refactor

--- administration/tail_log.sh
+++ administration/tail_log.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-echo "Running as $(whoami)"
+pgdata="$(psql "$DATABASE_URL" -c 'SHOW data_directory' --tuples-only | sed 's/^[ \t]*//')"
+echo "SHOW data_directory: $pgdata"
 
-echo "Find PostgreSQL data directory"
-pgdata="$(psql $DATABASE_URL -c 'SHOW data_directory' --tuples-only | sed 's/^[ \t]*//')"
-
-echo "Find current log file"
 pglog="$(psql -U postgres -c 'SELECT pg_current_logfile()' --tuples-only | awk '{print $1}')"
+echo "SELECT pg_current_logfile(): $pglog"
 
-full_path="$pgdata/$pglog"
-echo "full_path: $full_path"
-
-tail -f "$full_path"
+tail -f "$pgdata/$pglog"
