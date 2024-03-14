d8c08671f8986e12d2c5f94f49778203ed79313f
Andy Atkinson <andyatkinson@gmail.com>
2024-03-13 22:39:02 -0500

    Check the concurrent index build status
    Three total transactions:
    - One to add the index as INVALID
    - Two more passes building the index entries

---
+++ concurrent_index_build_progress.sql
@@ -0,0 +1,16 @@
+-- Check the status of the index build
+SELECT
+  now()::TIME(0),
+  a.query,
+  p.phase,
+  round(p.blocks_done / p.blocks_total::numeric * 100, 2) AS "% done",
+  p.blocks_total,
+  p.blocks_done,
+  p.tuples_total,
+  p.tuples_done,
+  ai.schemaname,
+  ai.relname,
+  ai.indexrelname
+FROM pg_stat_progress_create_index p
+JOIN pg_stat_activity a ON p.pid = a.pid
+LEFT JOIN pg_stat_all_indexes ai on ai.relid = p.relid AND ai.indexrelid = p.index_relid;
