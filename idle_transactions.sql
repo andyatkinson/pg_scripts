1dd4884afc0b65e2c71c9fd7a98bbb7bbd388459
Andy Atkinson <andyatkinson@gmail.com>
2024-06-27 18:38:45 -0500

    idle_in_transaction queries
    Query pg_stat_activity, for queries more than 5 minutes old using AGE()
    function

--- idle_transactions.sql
+++ idle_transactions.sql
@@ -3,3 +3,19 @@ SELECT *
 FROM pg_stat_activity
 WHERE (state = 'idle in transaction')
 AND xact_start IS NOT NULL;
+
+-- More fields, using age() function to find
+-- older transactions
+SELECT
+    pid,
+    usename,
+    state,
+    query,
+    age(clock_timestamp(), query_start) AS idle_duration,
+    client_addr
+FROM
+    pg_stat_activity
+WHERE
+    state = 'idle in transaction'
+    AND age(clock_timestamp(), query_start) > interval '5 minutes'; -- Adjust the interval as needed
+
