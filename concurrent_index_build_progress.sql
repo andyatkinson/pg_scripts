21a94e99dc3d4e9a91af2aafc826f5d722cd1b01
Andrew Atkinson <andyatkinson@gmail.com>
2025-12-19 11:07:21 -0600

    Update this query

--- concurrent_index_build_progress.sql
+++ concurrent_index_build_progress.sql
@@ -1,15 +1,24 @@
 -- Check the status of the index build
+-- Updates:
 SELECT
-  now()::TIME(0),
-  a.query,
-  p.phase,
-  round(p.blocks_done / p.blocks_total::numeric * 100, 2) AS "% done",
-  p.blocks_total,
-  p.blocks_done,
-  p.tuples_total,
-  p.tuples_done,
-  ai.schemaname,
-  ai.relname,
-  ai.indexrelname
-FROM pg_stat_progress_create_index p
-LEFT JOIN pg_stat_all_indexes ai on ai.relid = p.relid AND ai.indexrelid = p.index_relid;
+    age(clock_timestamp(), a.query_start) AS duration,
+    a.query,
+    p.phase,
+    round(p.blocks_done / NULLIF(p.blocks_total::numeric, 0) * 100, 2) AS "% blocks done",
+    p.blocks_total,
+    p.blocks_done,
+    round(p.tuples_done / NULLIF(p.tuples_total::numeric, 0) * 100, 2) AS "% tuples done",
+    p.tuples_total,
+    p.tuples_done,
+    p.command,
+    p.lockers_total,
+    p.lockers_done,
+    p.current_locker_pid,
+    index_relid::regclass AS index_name,
+    relid::regclass AS table_name,
+    pg_size_pretty(pg_relation_size(relid)) AS table_size,
+    a.pid
+FROM
+    pg_stat_progress_create_index p
+JOIN
+    pg_stat_activity a ON p.pid = a.pid;
